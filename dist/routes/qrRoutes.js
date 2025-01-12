338feec16ba1be0aa205aca0f3a73a2c310b21d9
Timothy Nuga <mac@Timothy-Nugas-MacBook-Pro.local>
2025-01-12 23:28:00 +0100

    Removed dist/ from upstream and updated .gitignore

--- dist/routes/qrRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const qrCodeController_1 = require("../controllers/qrCodeController");
-const router = (0, express_1.Router)();
-router.get('/', qrCodeController_1.getQRCode);
-exports.default = router;
